7fdc28d29ca1b5c89d705843885f0caafc62c4ba
nafaniaa <tgurinovich2005@gmail.com>
2026-02-03 17:41:03 +0300

    2nd query

--- sql-task/solutions.sql
+++ sql-task/solutions.sql
@@ -9,3 +9,19 @@ GROUP BY c.name
 ORDER BY film_count DESC;
 
 -- 2. Output the 10 actors whose movies rented the most, sorted in descending order.
+
+SELECT
+    actor.actor_id,
+    actor.first_name,
+    actor.last_name,
+    COUNT(rental.rental_id) AS rental_count
+FROM actor
+JOIN film_actor
+	ON actor.actor_id = film_actor.actor_id
+JOIN inventory
+    ON inventory.film_id = film_actor.film_id
+JOIN rental
+    ON rental.inventory_id = inventory.inventory_id
+GROUP BY actor.actor_id, actor.first_name, actor.last_name
+ORDER BY rental_count DESC
+LIMIT 10;
