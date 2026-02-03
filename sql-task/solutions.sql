653bd537176f8a2e52ae0296667889c46281f25e
nafaniaa <tgurinovich2005@gmail.com>
2026-02-03 19:10:19 +0300

    5th query

--- sql-task/solutions.sql
+++ sql-task/solutions.sql
@@ -55,3 +55,41 @@ LEFT JOIN inventory
     ON inventory.film_id = film.film_id
 WHERE inventory.inventory_id IS NULL;
 
+
+-- 5. Output the top 3 actors who have appeared the most in movies in the “Children” category. 
+-- If several actors have the same number of movies, output all of them.
+
+WITH actor_children_films AS (
+SELECT 
+	actor.actor_id,
+	actor.first_name,
+	actor.last_name,
+	COUNT(film_category.film_id) AS film_count
+FROM actor
+	JOIN film_actor
+        ON film_actor.actor_id = actor.actor_id
+    JOIN film_category
+        ON film_category.film_id = film_actor.film_id
+    JOIN category
+        ON category.category_id = film_category.category_id
+    WHERE category.name = 'Children'
+    GROUP BY actor.actor_id, actor.first_name, actor.last_name
+),
+ranked AS(
+	SELECT 
+        actor_id,
+        first_name,
+        last_name,
+        film_count,
+        DENSE_RANK() OVER (ORDER BY film_count DESC) AS rank_position
+    FROM actor_children_films
+)
+SELECT 
+    actor_id,
+    first_name,
+    last_name,
+    film_count
+FROM ranked
+WHERE rank_position <= 3
+ORDER BY film_count DESC;
+	
